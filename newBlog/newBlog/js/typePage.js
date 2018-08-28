a697443803d2d033ce0ab6ac3f2847220e60b7c2
nalixu <18867836989@163.com>
2018-08-28 18:06:34 +0800

    blog v1.0

--- newBlog/newBlog/js/typePage.js
+++ newBlog/newBlog/js/typePage.js
@@ -1,4 +1,50 @@
 var data=localStorage.data?JSON.parse(localStorage.data):[];
-function render(){
-    
+
+function getQueryString(name){
+    var str=location.search.substring(1);
+    var arr=str.split('&');
+    var obj={};
+    for(var i=0;i<arr.length;i++){
+        var key=arr[i].split('=')[0];
+        var val=arr[i].split('=')[1];
+        obj[key]=val;
+    }
+    if(name){
+        return obj[name];
+    }else{
+        return obj;
+    }
 }
+let type=getQueryString('type');
+let articleList=data.find(item=>item.type==type);
+function render(data){
+    var container=document.querySelector('.container');
+    document.querySelector('.list').remove();
+    var list=document.createElement('div');
+    list.className='list';
+    
+    data.forEach(val=>{
+        var article=document.createElement('div');
+        article.className='article';
+        article.innerHTML=`
+        <img src="${val.image}" alt="1" />
+        <div class="articleContent">
+            <div class="title"><a>${val.title}</a></div>
+            <div class="articleNote">
+                <div class="noteLeft">By<a>REIC KARKOVACK</a>${val.createTime}</div>
+                <div class="noteRight"><a>${val.type}</a></div>
+            </div>
+            <div class="articleText">
+               ${val.content}
+            </div>
+        </div>
+        `
+        list.append(article);
+        
+    })
+   container.append(list);
+   
+}
+
+render(data)
+render(articleList);
